8f7c5fa7c0834d4af5ceed29149c8a4bab647fbf
Nabin Karki <karki0008@gmail.com>
2025-05-21 09:09:05 +0545

    readme file has been updated

--- day17/diagonalTest.js
+++ day17/diagonalTest.js
@@ -1,12 +1,5 @@
-function diagonalDifference(arr) {
-  let leftDifference = 0;
-  let rightDifference = 0;
-  for (let i = 0; i < arr.length; i++) {
-    leftDifference = arr[i][i];
-    rightDifference = arr[i][arr.length - (i + 1)];
-  }
-  return Math.abs(leftDifference - rightDifference);
-}
+
+
 let arr = [
   [1, 2, 1],
   [20, 2, 34],
