c3034c4c1d34791cb0774582fedf1fcbe5c68337
Nabin Karki <karki0008@gmail.com>
2025-05-28 21:47:36 +0545

    basic of DSA7  has been practised

--- day19/squareOfEveryDigit.js
+++ day19/squareOfEveryDigit.js
@@ -1,14 +1,26 @@
-function squareOfEveryDigits(n) {
-  while (n >= 10) {
-    let output = 0;
-    while (n != 0) {
-      rem = n % 10;
-      n = Math.floor(n / 10);
-      output = output + rem ** 2;
+// function squareOfEveryDigits(n) {
+//   while (n >= 10) {
+//     let output = 0;
+//     while (n != 0) {
+//       rem = n % 10;
+//       n = Math.floor(n / 10);
+//       output = output + rem ** 2;
+//     }
+//     n = output;
+//   }
+//   return n;
+// }
+// let n = 45;
+// console.log(squareOfEveryDigits(n));
+
+function maximum(array) {
+  let max = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > max) {
+      max = array[i];
     }
-    n = output;
   }
-  return n;
+  return max;
 }
-let n = 45;
-console.log(squareOfEveryDigits(n));
+let array = [1, 5, 64, 2, 67];
+console.log(maximum(array));
